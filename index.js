f32ce5b576ab93a2750bcead794cd4f67d130b16
chimZuk <dzmitry.kuzmitch@gmail.com>
2020-02-18 17:24:03 -0500

    Added SSL certificate for scrawlless.com

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 const http = require('http');
+const https = require('https');
 const express = require('express');
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
+const fs = require('fs');
 const app = express();
 
 require('./database/database');
 const APIRoutes = require('./routes/api');
 
 app.use('/', express.static(__dirname + '/dist'));
+app.get('*', (req, res) => {
+    res.redirect('/');
+});
 
 app.use(function (req, res, next) {
     res.setHeader('Access-Control-Allow-Origin', '*');
@@ -22,6 +27,14 @@ app.use(bodyParser.json());
 app.use('/api', APIRoutes);
 
 const httpServer = http.createServer(app);
-httpServer.listen(8080, () => {
-    console.log('HTTP Server running on port 8080');
+httpServer.listen(80, () => {
+    console.log('HTTP Server running on port 80');
+});
+
+const httpsServer = https.createServer({
+    key: fs.readFileSync('/etc/letsencrypt/live/scrawlless.com/privkey.pem'),
+    cert: fs.readFileSync('/etc/letsencrypt/live/scrawlless.com/fullchain.pem'),
+}, app);
+httpsServer.listen(443, () => {
+    console.log('HTTPS Server running on port 443');
 });
