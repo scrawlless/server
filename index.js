66e2fb35c8c8a91e3edecd49a99fa102871fc3a9
DZMITRY KUZMITCH <kuzya19989@outlook.com>
2020-02-18 09:48:36 -0500

    Inported database config

--- index.js
+++ index.js
@@ -3,9 +3,7 @@ const express = require('express');
 const bodyParser = require('body-parser')
 const app = express();
 
-const mongoose = require('mongoose');
-const Test = mongoose.model('Test');
-
+require('./database/database');
 const APIRoutes = require('./routes/api');
 
 app.use('/', express.static(__dirname + '/dist'));
