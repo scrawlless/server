2da15d4b1630a611dcd6b3a51bbfbc23e1ed82b9
tom <tslawdev@gmail.com>
2020-02-23 01:34:14 +0000

    little cleanup

--- controllers/user.js
+++ controllers/user.js
@@ -7,42 +7,39 @@ const jwt = require('jsonwebtoken');
 const secret = process.env.JWT_SECRET;
 console.log(`secret: ${secret}`);
 
-module.exports.register = (req, res) => {
+module.exports.register = async (req, res) => {
 	var pass = req.body.pass;
-	bcrypt.genSalt(12, (err, salt) => {
-		bcrypt.hash(pass, salt, (err, hash) => {
-			var user = new User({
-				name: req.body.name,
-				username: req.body.username,
-				email: req.body.email,
-				hash,
-			});
+	var salt = await bcrypt.genSalt(12);
+	var hash = await bcrypt.hash(pass, salt);
 
-			user.save(err => {
-				if(err) return res.send({ error: err });
-				res.send({
-					jwt: jwt.sign({ username: req.body.username }, secret)
-				});
-			});
-		});
+	var user = new User({
+		name: req.body.name,
+		username: req.body.username,
+		email: req.body.email,
+		hash
 	});
+
+	user.save(err => {
+		if (err) return res.send({ error: err });
+		res.send({ jwt: jwt.sign({ username: req.body.username }, secret) });
+	});
+
 };
 
 module.exports.login = (req, res) => {
 	var username = req.body.username;
 	var pass = req.body.pass;
 
-	User.find({ 'username': username }, (err, user) => {
+	User.find({ 'username': username }, async (err, user) => {
 		if (err) return res.send(err);
-		bcrypt.compare(pass, user[0].hash, (err, result) => {
-			if(result){
-				return res.send({
-					jwt: jwt.sign({ username: req.body.username }, secret),
-					login: true
-				});
-			}
-			res.send(401, "User not authenticated!");
-		});
-	});
 
+		var result = await bcrypt.compare(pass, user[0].hash);
+		if(result){
+			return res.send({
+				jwt: jwt.sign({ username: req.body.username }, secret),
+				login: true
+			});
+		}
+		res.send(401, "User not authenticated!");
+	});
 };
